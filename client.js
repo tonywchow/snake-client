f6f3189a3245ab93f86bb520a85bafb8ad85087c
Tony Chow <tchow9@gmail.com>
2023-02-20 10:37:26 -0800

    Logging connection made and sending Name to server

--- client.js
+++ client.js
@@ -9,11 +9,20 @@ const connect = () => {
   //interpret incoming data as text
   conn.setEncoding('utf8');
   
+  conn.on('connect', () => {
+      console.log('Successfully connected to game server!')
+      conn.write('Name: TWC')
+    }
+  )
+  
   conn.on('data', (data) => {
     //code that does something
     console.log('Server says: ', data)
   });
+
   return conn;
 };
+
+
 
 module.exports = {connect};
