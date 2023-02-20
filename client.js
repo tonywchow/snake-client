78830552292cb01007fb4021252dc95ad304ccca
Tony Chow <tchow9@gmail.com>
2023-02-20 10:06:30 -0800

    Using ES6 ShortHand Syntax

---
+++ client.js
@@ -0,0 +1,19 @@
+//establishes a connecction with the game server
+const net = require('net');
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost',//IP address here,
+    port: 50541, //PORT number here,
+  });
+
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+  
+  conn.on('data', (data) => {
+    //code that does something
+    console.log('Server says: ', data)
+  });
+  return conn;
+};
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require('net');
-
-//establishes a connecction with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',//IP address here,
-    port: 50541, //PORT number here,
-  });
-
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-  
-  conn.on('data', (data) => {
-    //code that does something
-    console.log('Server says: ', data)
-  });
-  return conn;
-};
+const {connect} = require('./client')
 
 ////structure as event handler:
 // conn.on('connect', () => {
