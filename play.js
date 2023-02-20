c9c47fa8f04be05b0b13ba467400aa175992a8e9
Tony Chow <tchow9@gmail.com>
2023-02-20 11:18:01 -0800

    Added handleUserInput function

--- play.js
+++ play.js
@@ -8,3 +8,19 @@ const {connect} = require('./client')
 console.log('Connecting ...');
 connect();
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+
+setupInput();
