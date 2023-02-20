0d6b706c2c2e39f127db488417fa8c12db7b709c
Tony Chow <tchow9@gmail.com>
2023-02-20 11:30:31 -0800

    re-organizing code

--- play.js
+++ play.js
@@ -13,14 +13,12 @@ const setupInput = function() {
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  const handleUserInput = (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  }
   stdin.on('data', handleUserInput);
   return stdin;
 }
-
-
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
 setupInput();
